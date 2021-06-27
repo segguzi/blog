7db0a392b5d03b5dbe14356c72e94057ae0f9ee8
segguzi <86434080+segguzi@users.noreply.github.com>
2021-06-27 18:01:08 -0500

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,6 +13,7 @@ export async function getStaticProps() {
     }
   }
 }
+
 export default function Home({ allPostsData }) {
   return (
     <Layout home>
@@ -25,11 +26,6 @@ export default function Home({ allPostsData }) {
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
-		<p>
-			<Link href="/posts/first-post">
-				<a>To my first post</a>
-			</Link>
-		</p>
       </section>
 	  <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
         <h2 className={utilStyles.headingLg}>Blog</h2>
